6c63dfccf29122a744fcbc3ccbcfd289576d3bca
Dominic Hains <dominic.hains@shopify.com>
2022-06-03 13:30:22 -0400

    fix: path of the build script

--- benchmark/build.sh
+++ benchmark/build.sh
@@ -7,8 +7,7 @@ for d in ./*_function/
 do 
   cd "$d"
   CARGO_TARGET_DIR=./target cargo build --profile "benchmark" --target "wasm32-wasi"
-  echo $d
-  cp -n ./target/wasm32-wasi/release/*.wasm ../build/ || true # avoids the error that -n throws if the file already exists
+  cp -n ./target/wasm32-wasi/benchmark/*.wasm ../build/ || true # avoids the error that -n throws if the file already exists
   cd ..
 done
 
