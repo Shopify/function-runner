bd51a8223831ff68d2f7f07aaec66d0ad85f48dc
David Cameron <david.cameron@shopify.com>
2024-01-03 17:39:49 -0500

    Closer but not working

--- src/engine.rs
+++ src/engine.rs
@@ -1,8 +1,14 @@
 use anyhow::{anyhow, Result};
+use clap::error;
 use rust_embed::RustEmbed;
+use wasmtime::AsContextMut;
+use wasmtime::component::*;
+use wasmtime_wasi::preview2::command::sync::add_to_linker;
+use wasmtime_wasi::preview2::{command, Table, WasiCtx, WasiCtxBuilder, WasiView};
 use std::{collections::HashSet, io::Cursor, path::PathBuf};
-use wasi_common::{I32Exit, WasiCtx};
-use wasmtime::{AsContextMut, Config, Engine, Linker, Module, Store, Caller, component::Component};
+use wasi_common::{I32Exit};
+//use wasmtime::{AsContextMut, Config, Engine, Module, Store, Caller, component::Component};
+use wasmtime::{Store, Caller, Module, Engine, Config};
 use crate::local_storage::runner::local_storage::sql_ops;
 
 use crate::{
@@ -24,34 +30,65 @@ pub struct ProfileOpts {
 #[folder = "providers/"]
 struct StandardProviders;
 
-fn import_modules(
-    module: &Module,
-    engine: &Engine,
-    linker: &mut Linker<WasiCtx>,
-    mut store: &mut Store<WasiCtx>,
-) {
-    let imported_modules: HashSet<String> =
-        module.imports().map(|i| i.module().to_string()).collect();
-    imported_modules.iter().for_each(|imported_module| {
-        let imported_module_bytes = StandardProviders::get(&format!("{imported_module}.wasm"));
-
-        if let Some(bytes) = imported_module_bytes {
-            let imported_module = Module::from_binary(engine, &bytes.data)
-                .unwrap_or_else(|_| panic!("Failed to load module {imported_module}"));
-
-            let imported_module_instance = linker
-                .instantiate(&mut store, &imported_module)
-                .expect("Failed to instantiate imported instance");
-            linker
-                .instance(
-                    &mut store,
-                    "javy_quickjs_provider_v1",
-                    imported_module_instance,
-                )
-                .expect("Failed to import module");
-        }
-    });
+pub struct Ctx {
+    table: Table,
+    sql: SQLStorage,
+    wasi: WasiCtx,
 }
+
+impl Ctx {
+    fn new(sql: SQLStorage, wasi: WasiCtx) -> Self {
+        let table = Table::new();
+
+        Self { table, sql, wasi }
+    }
+}
+
+impl WasiView for Ctx {
+    fn table(&self) -> &Table {
+        &self.table
+    }
+
+    fn table_mut(&mut self) -> &mut Table {
+        &mut self.table
+    }
+
+    fn ctx(&self) -> &WasiCtx {
+        &self.wasi
+    }
+
+    fn ctx_mut(&mut self) -> &mut WasiCtx {
+        &mut self.wasi
+    }
+}
+
+// fn import_modules(
+//     module: &Component,
+//     engine: &Engine,
+//     linker: &mut Linker<Ctx>,
+//     mut store: &mut Store<Ctx>,
+// ) {
+//     let imported_modules: HashSet<String> =
+//         module.imports().map(|i| i.module().to_string()).collect();
+//     imported_modules.iter().for_each(|imported_module| {
+//         let imported_module_bytes = StandardProviders::get(&format!("{imported_module}.wasm"));
+
+//         if let Some(bytes) = imported_module_bytes {
+//             let imported_module = Component::from_binary(engine, &bytes.data)
+//                 .unwrap_or_else(|_| panic!("Failed to load module {imported_module}"));
+//             let imported_module_instance = linker
+//                 .instantiate(&mut store, &imported_module)
+//                 .expect("Failed to instantiate imported instance");
+//             // linker
+//             //     .instance(
+//             //         &mut store,
+//             //         "javy_quickjs_provider_v1",
+//             //         imported_module_instance,
+//             //     )
+//             //     .expect("Failed to import module");
+//         }
+//     });
+// }
 
 pub fn run(
     function_path: PathBuf,
@@ -68,12 +105,12 @@ pub fn run(
     let engine = Engine::new(
         &config)?;
 
-    let module = Module::from_file(&engine, &function_path)
+    let module = Component::from_file(&engine, &function_path)
         .map_err(|e| anyhow!("Couldn't load the Function {:?}: {}", &function_path, e))?;
 
-    let input_stream = wasi_common::pipe::ReadPipe::new(Cursor::new(input));
-    let output_stream = wasi_common::pipe::WritePipe::new_in_memory();
-    let error_stream = wasi_common::pipe::WritePipe::new(LogStream::default());
+    // let input_stream = wasi_common::pipe::ReadPipe::new(Cursor::new(input));
+    // let output_stream = wasi_common::pipe::WritePipe::new_in_memory();
+    // let error_stream = wasi_common::pipe::WritePipe::new(LogStream::default());
 
     let memory_usage: u64;
     let instructions: u64;
@@ -81,43 +118,44 @@ pub fn run(
     let profile_data: Option<String>;
 
     {
+        let sql = SQLStorage::new("demo.sqlite");
         let mut linker = Linker::new(&engine);
-        wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
-        let wasi = deterministic_wasi_ctx::build_wasi_ctx();
-        wasi.set_stdin(Box::new(input_stream));
-        wasi.set_stdout(Box::new(output_stream.clone()));
-        wasi.set_stderr(Box::new(error_stream.clone()));
-        let mut store = Store::new(&engine, wasi);
-        store.add_fuel(u64::MAX)?;
+        //let wasi = deterministic_wasi_ctx::build_wasi_ctx();
+        let mut wasi = WasiCtxBuilder::new()
+        .inherit_stdio()
+        .build();
+        // wasi.set_stdin(Box::new(input_stream));
+        // wasi.set_stdout(Box::new(output_stream.clone()));
+        // wasi.set_stderr(Box::new(error_stream.clone()));
+        let mut store = Store::new(&engine, Ctx::new(sql, wasi));
+       //store.add_fuel(u64::MAX)?;
         store.set_epoch_deadline(1);
 
-        let sql_storage = SQLStorage::new("demo.sqlite");
+        //import_modules(&module, &engine, &mut linker, &mut store);
+        sql_ops::add_to_linker(&mut linker, |ctx: &mut Ctx| &mut ctx.sql)?;
+        add_to_linker(&mut linker)?;
 
-        import_modules(&module, &engine, &mut linker, &mut store);
 
-        sql_ops::add_to_linker(&mut linker, |ctx| -> &mut SQLStorage {
-            ctx.data.as_mut().unwrap()
-        })?;
-
-        linker.module(&mut store, "Function", &module)?;
-        let instance = linker.instantiate(&mut store, &module)?;
+        //linker.module(&mut store, "Function", &module)?;
+        let (_, instance) = Sql::instantiate(&mut store, &module, &linker)?;
+        //let instance = linker.instantiate(&mut store, &module)?;
 
         let func = instance.get_typed_func::<(), ()>(store.as_context_mut(), export)?;
 
-        let module_result;
-        (module_result, profile_data) = if let Some(profile_opts) = profile_opts {
-            let (result, profile_data) = wasmprof::ProfilerBuilder::new(&mut store)
-                .frequency(profile_opts.interval)
-                .weight_unit(wasmprof::WeightUnit::Fuel)
-                .profile(|store| func.call(store.as_context_mut(), ()));
+        //let module_result;
+        // (module_result, profile_data) = if let Some(profile_opts) = profile_opts {
+        //     let (result, profile_data) = wasmprof::ProfilerBuilder::new(&mut store)
+        //         .frequency(profile_opts.interval)
+        //         .weight_unit(wasmprof::WeightUnit::Fuel)
+        //         .profile(|store| func.call(store.as_context_mut(), ()));
 
-            (
-                result,
-                Some(profile_data.into_collapsed_stacks().to_string()),
-            )
-        } else {
-            (func.call(store.as_context_mut(), ()), None)
-        };
+        //     (
+        //         result,
+        //         Some(profile_data.into_collapsed_stacks().to_string()),
+        //     )
+        // } else {
+            let module_result = func.call(store.as_context_mut(), ());
+        //};
 
         // modules may exit with a specific exit code, an exit code of 0 is considered success but is reported as
         // a GuestFault by wasmtime, so we need to map it to a success result. Any other exit code is considered
@@ -132,21 +170,23 @@ pub fn run(
         // This is a hack to get the memory usage. Wasmtime requires a mutable borrow to a store for caching.
         // We need this mutable borrow to fall out of scope so that we can measure memory usage.
         // https://docs.rs/wasmtime/0.37.0/wasmtime/struct.Instance.html#why-does-get_export-take-a-mutable-context
-        let memory_names: Vec<String> = instance
-            .exports(&mut store)
-            .filter(|export| export.clone().into_memory().is_some())
-            .map(|export| export.name().to_string())
-            .collect();
+        // let memory_names: Vec<String> = instance
+        //     .exports(&mut store)
+        //     .filter(|export| export.clone().into_memory().is_some())
+        //     .map(|export| export.name().to_string())
+        //     .collect();
 
-        memory_usage = memory_names
-            .iter()
-            .map(|name| {
-                let memory = instance.get_memory(&mut store, name).unwrap();
-                memory.data_size(&store) as u64
-            })
-            .sum::<u64>()
-            / 1024;
-        instructions = store.fuel_consumed().unwrap_or_default();
+        // memory_usage = memory_names
+        //     .iter()
+        //     .map(|name| {
+        //         let memory = instance.get_memory(&mut store, name).unwrap();
+        //         memory.data_size(&store) as u64
+        //     })
+        //     .sum::<u64>()
+        //     / 1024;
+        memory_usage = 0;
+        instructions = 0;
+        //store.fuel_consumed().unwrap_or_default();
 
         match module_result {
             Ok(_) => {}
@@ -156,12 +196,16 @@ pub fn run(
         }
     };
 
+    let error_stream = wasi_common::pipe::WritePipe::new(LogStream::default());
+
     let mut logs = error_stream
         .try_into_inner()
         .expect("Log stream reference still exists");
 
     logs.append(error_logs.as_bytes())
         .expect("Couldn't append error logs");
+
+    let output_stream = wasi_common::pipe::WritePipe::new_in_memory();
 
     let raw_output = output_stream
         .try_into_inner()
@@ -189,7 +233,7 @@ pub fn run(
         instructions,
         logs.to_string(),
         output,
-        profile_data,
+        None,
     );
 
     Ok(function_run_result)
