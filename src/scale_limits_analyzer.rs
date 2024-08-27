2c07b6e12b76f1589df5ef066330d45230b99110
Mehdi Salemi <mehdi.salemi@shopify.com>
2024-08-27 15:56:27 +0000

    code review updates

--- src/scale_limits_analyzer.rs
+++ src/scale_limits_analyzer.rs
@@ -3,7 +3,7 @@ use bluejay_core::definition::SchemaDefinition as CoreSchemaDefinition;
 use bluejay_core::AsIter;
 use bluejay_core::Directive;
 use bluejay_core::Value as CoreValue;
-use bluejay_core::{definition::HasDirectives, ValueReference};
+use bluejay_core::{definition::prelude::*, ValueReference};
 use bluejay_parser::error::{Annotation, Error as BluejayError};
 use bluejay_parser::{
     ast::{
@@ -29,20 +29,8 @@ pub type ScaleLimitsAnalyzer<'a> = bluejay_validator::executable::operation::Orc
 pub struct ScaleLimits<'a> {
     input: &'a Value,
     value_stack: Vec<Vec<&'a Value>>,
-    rates: HashMap<String, f64>,
-}
-
-impl<'a> ScaleLimits<'a> {
-    pub fn rates_as_json(&self) -> String {
-        serde_json::to_string(&self.rates).unwrap_or_else(|_| "{}".to_string())
-    }
-
-    pub fn update_max_rate(&mut self, field_name: &str, rate: f64) {
-        let entry = self.rates.entry(field_name.to_string()).or_insert(0.0);
-        if rate > *entry {
-            *entry = rate;
-        }
-    }
+    path_stack: Vec<&'a str>,
+    rates: HashMap<Vec<&'a str>, f64>,
 }
 
 impl<'a>
@@ -57,7 +45,7 @@ impl<'a>
 
     fn new(
         operation_definition: &'a <ExecutableDocument as bluejay_core::executable::ExecutableDocument>::OperationDefinition,
-        schema_definition: &'a SchemaDefinition,
+        schema_definition: &'a SchemaDefinition<'a>,
         variable_values: &'a serde_json::Map<String, serde_json::Value>,
         cache: &'a bluejay_validator::executable::Cache<'a, ExecutableDocument, SchemaDefinition>,
         extra_info: &'a Value,
@@ -65,46 +53,68 @@ impl<'a>
         Self {
             input: extra_info,
             value_stack: vec![vec![extra_info]],
+            path_stack: Vec::new(),
             rates: Default::default(),
         }
     }
 
     fn visit_field(
         &mut self,
-        field: &'_ <ExecutableDocument<'_> as bluejay_core::executable::ExecutableDocument>::Field,
+        field: &'a <ExecutableDocument<'a> as bluejay_core::executable::ExecutableDocument>::Field,
         field_definition: &'_ <SchemaDefinition as CoreSchemaDefinition>::FieldDefinition,
-        scoped_type: bluejay_core::definition::TypeDefinitionReference<
+        _scoped_type: bluejay_core::definition::TypeDefinitionReference<
             '_,
             <SchemaDefinition<'_> as CoreSchemaDefinition>::TypeDefinition,
         >,
-        included: bool,
+        _included: bool,
     ) {
+        self.path_stack.push(field.response_key());
         let rate = Self::rate_for_field_definition(field_definition);
         let values = self.value_stack.last().unwrap();
         let mut nested_values = Vec::new();
         values.iter().for_each(|value| {
-            let values_for_field = match value {
-                Value::Object(object) => object.get(field.response_key()).into_iter().collect(),
-                Value::Null => Vec::new(),
-                Value::Array(list) => list.iter().collect(),
+            let value_for_field = match value {
+                Value::Object(object) => object.get(field.response_key()),
+                Value::Null => None,
                 _ => panic!("invalid value"),
             };
             // TODO handle scaling for this value
+            if let Some(rate) = rate {
+                let length = match value_for_field {
+                    Some(Value::String(s)) => s.len(),
+                    Some(Value::Array(arr)) => arr.len(),
+                    _ => 1,
+                };
+                let increment = length as f64 * rate;
 
-            nested_values.extend(values_for_field);
+                if let Some(cumulative_rate_for_path) = self.rates.get_mut(&self.path_stack) {
+                    *cumulative_rate_for_path += increment;
+                } else {
+                    self.rates.insert(self.path_stack.clone(), increment);
+                }
+            }
+
+            match value_for_field {
+                Some(Value::Array(values)) => nested_values.extend(values),
+                Some(value) => nested_values.push(value),
+                None => {}
+            }
         });
+
+        self.value_stack.push(nested_values);
     }
 
     fn leave_field(
         &mut self,
-        field: &'a <ExecutableDocument<'a> as bluejay_core::executable::ExecutableDocument>::Field,
-        field_definition: &'a <SchemaDefinition<'a> as CoreSchemaDefinition>::FieldDefinition,
-        scoped_type: bluejay_core::definition::TypeDefinitionReference<
+        _field: &'a <ExecutableDocument<'a> as bluejay_core::executable::ExecutableDocument>::Field,
+        _field_definition: &'a <SchemaDefinition<'a> as CoreSchemaDefinition>::FieldDefinition,
+        _scoped_type: bluejay_core::definition::TypeDefinitionReference<
             'a,
             <SchemaDefinition<'a> as CoreSchemaDefinition>::TypeDefinition,
         >,
-        included: bool,
+        _included: bool,
     ) {
+        self.path_stack.pop().unwrap();
         self.value_stack.pop().unwrap();
     }
 }
@@ -120,11 +130,17 @@ impl<'a>
     type Output = f64;
 
     fn into_output(self) -> Self::Output {
-        0.0
+        self.rates
+            .into_values()
+            .fold(Self::MIN_SCALE_FACTOR, f64::max)
+            .clamp(Self::MIN_SCALE_FACTOR, Self::MAX_SCALE_FACTOR)
     }
 }
 
 impl<'a> ScaleLimits<'a> {
+    const MIN_SCALE_FACTOR: f64 = 1.0;
+    const MAX_SCALE_FACTOR: f64 = 10.0;
+
     fn rate_for_field_definition(
         field_definition: &FieldDefinition<DefaultContext>,
     ) -> Option<f64> {
