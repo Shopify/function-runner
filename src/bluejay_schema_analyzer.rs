c5d4a9cde0c48e9e3cd621a174e610da73382c93
Mehdi Salemi <mehdi.salemi@shopify.com>
2024-09-04 14:16:23 +0000

    test from pairing

--- src/bluejay_schema_analyzer.rs
+++ src/bluejay_schema_analyzer.rs
@@ -161,4 +161,38 @@ mod tests {
             "The scale factor did not match the expected value, indicating potential double counting"
         );
     }
+
+    #[test]
+    fn test_no_double_counting_for_duplicate_fields_with_nested_array() {
+        let schema_string = r#"
+            directive @scaleLimits(rate: Float!) on FIELD_DEFINITION
+            type Query {
+                field: [MyObject]
+            }
+
+            type MyObject {
+                field: [String] @scaleLimits(rate: 0.005)
+            }
+        "#;
+        let query = "{ field { field } }";
+        let nested_field = json!({ "field": vec!["value"; 200] });
+        let input_json = json!({
+            "field": vec![nested_field; 2]
+        });
+
+        let result =
+            BluejaySchemaAnalyzer::analyze_schema_definition(schema_string, query, &input_json);
+        assert!(
+            result.is_ok(),
+            "Expected successful analysis but got an error: {:?}",
+            result
+        );
+
+        let scale_factor = result.unwrap();
+        let expected_scale_factor = 2.0;
+        assert_eq!(
+            scale_factor, expected_scale_factor,
+            "The scale factor did not match the expected value, indicating potential double counting"
+        );
+    }
 }
