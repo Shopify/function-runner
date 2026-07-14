c9db74074b9021436954bddc675bec43b0d9b135
Jeff Charles <jeff.charles@shopify.com>
2026-07-14 14:57:07 -0400

    Include other separators

--- src/container.rs
+++ src/container.rs
@@ -13,11 +13,24 @@ impl serde_json::ser::Formatter for ShopifyJsonFormatter {
         W: ?Sized + io::Write,
     {
         let mut start = 0;
-        for (index, byte) in fragment.bytes().enumerate() {
-            if byte == b'/' {
-                writer.write_all(&fragment.as_bytes()[start..index])?;
-                writer.write_all(br"\/")?;
-                start = index + 1;
+        for (index, character) in fragment.char_indices() {
+            match character {
+                '/' => {
+                    writer.write_all(&fragment.as_bytes()[start..index])?;
+                    writer.write_all(br"\/")?;
+                    start = index + character.len_utf8();
+                }
+                '\u{2028}' => {
+                    writer.write_all(&fragment.as_bytes()[start..index])?;
+                    writer.write_all(br"\u2028")?;
+                    start = index + character.len_utf8();
+                }
+                '\u{2029}' => {
+                    writer.write_all(&fragment.as_bytes()[start..index])?;
+                    writer.write_all(br"\u2029")?;
+                    start = index + character.len_utf8();
+                }
+                _ => {}
             }
         }
         writer.write_all(&fragment.as_bytes()[start..])
@@ -181,6 +194,22 @@ mod tests {
         assert_eq!(
             String::from_utf8(container.raw).unwrap(),
             r#"{"id":"gid:\/\/shopify\/Product\/1"}"#
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn json_input_escapes_line_and_paragraph_separators_like_shopify_json() -> Result<()> {
+        let input = "{\"line\":\"before\u{2028}after\",\"paragraph\":\"before\u{2029}after\"}"
+            .as_bytes()
+            .to_vec();
+
+        let container = BytesContainer::new(BytesContainerType::Input, Codec::Json, input)?;
+
+        assert_eq!(
+            String::from_utf8(container.raw).unwrap(),
+            r#"{"line":"before\u2028after","paragraph":"before\u2029after"}"#
         );
 
         Ok(())
